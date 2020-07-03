79913f9ebf7c0694a0adad8f11262447e8664800
bernyfrancois <itoutouberny@gmail.com>
2020-07-03 22:01:55 +0200

    finalise contact form html

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -6,20 +6,41 @@ import github from "../files/github.png";
 class Contact extends Component {
     constructor(props) {
         super(props);
-        this.state = {value: 'coconut'};
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = {
+            name: undefined,
+            email: undefined,
+            service: undefined,
+            budget: undefined,
+            content: undefined,
+        };
     }
 
-    handleChange(event) {
+    handleChange = (event) => {
         this.setState({value: event.target.value});
     }
+    handleTxtChange = (event) => {
+        this.setState({value: event.target.value});
+        this.growHeight(event.target)
+    }
 
-    handleSubmit(event) {
-        alert('Votre parfum favori est : ' + this.state.value);
+
+    handleSubmit = (event) => {
+        alert(this.setState(
+            {
+                name: undefined,
+                email: undefined,
+                service: undefined,
+                budget: undefined,
+                content: undefined,
+            }
+        ));
         event.preventDefault();
     }
+
+    growHeight = (target) => {
+        target.style.height = target.scrollHeight + 'px';
+    }
+
     render() {
         return (
             <section className={"contact"}>
@@ -31,15 +52,15 @@ class Contact extends Component {
                     <span className="subtitle">
                         Contact
                     </span>
-                        <h2 className={"title"}>Let's do something together</h2>
+                        <h2 className={"title"}>Get in touch — let’s work together.</h2>
                     </div>
                     <div className="social-links">
                     <span className="subtitle">
                         Social networks
                     </span>
-                        <ul className={"social-links"}>
-                            <li><a href="https://www.linkedin.com/in/fran%C3%A7ois-itoutou-652a2917a/">François Itoutou <span className="social-icon"><img src={linkedin} alt="icon"/></span></a></li>
-                            <li><a href="https://github.com/bernyfrancois">bernyfrancois <span className="social-icon"><img src={github} alt="icon"/></span></a></li>
+                        <ul>
+                            <li><a href="https://www.linkedin.com/in/fran%C3%A7ois-itoutou-652a2917a/">François Itoutou <img className="social-icon" src={linkedin} alt="icon"/></a></li>
+                            <li><a href="https://github.com/bernyfrancois">bernyfrancois <img className="social-icon" src={github} alt="icon"/></a></li>
                         </ul>
                     </div>
                 </div>
@@ -47,15 +68,28 @@ class Contact extends Component {
                     <h3>Got a project ? Leave me a message if you want to work together on something exciting. Web or Mobile.</h3>
                     <form onSubmit={this.handleSubmit}>
                         <label>
-                            Choisissez votre parfum favori :
-                            <select value={this.state.value} onChange={this.handleChange}>
-                                <option value="grapefruit">Pamplemousse</option>
-                                <option value="lime">Citron vert</option>
-                                <option value="coconut">Noix de coco</option>
-                                <option value="mango">Mangue</option>
-                            </select>
+                            <span className="label-title">Your name</span>
+                            <input type="text" className={"single-bar"} value={this.state.name} onChange={this.handleChange} placeholder={"What's your name?"}/>
                         </label>
-                        <input type="submit" value="Envoyer" />
+                        <label>
+                            <span className="label-title">Your email</span>
+                            <input type="email" className={"single-bar"} value={this.state.email} onChange={this.handleChange} placeholder={"What's your email address?"}/>
+                        </label>
+                        <label>
+                            <span className="label-title">Object</span>
+                            <input type="text" className={"single-bar"} value={this.state.service} onChange={this.handleChange} placeholder={"What's can i do for you?"}/>
+                        </label>
+                        <label>
+                            <span className="label-title">Budget</span>
+                            <input type="text" className={"single-bar"} value={this.state.budget} onChange={this.handleChange} placeholder={"What's your budget?"}/>
+                        </label>
+                        <label className={"message"}>
+                            <span className="label-title">Message</span>
+                            <textarea value={this.state.content} onChange={this.handleTxtChange} placeholder={"What's your message?"}/>
+                        </label>
+                        <div className="btn-container">
+                            <button type={"sudmit"} className="btn">Send message</button>
+                        </div>
                     </form>
                 </div>
             </section>
